43e16de8eec9b85b9a0b6dd9cf6626a857b911d7
bytebitgo <bytebit@foxmail.com>
2022-02-18 10:07:06 +0800

    release

--- index.go
+++ index.go
@@ -1,10 +1,16 @@
 /*
  * @Author: bytebit
  * @Date: 2022-02-17 19:28:31
- * @LastEditTime: 2022-02-17 19:28:31
+ * @LastEditTime: 2022-02-18 10:06:00
  * @LastEditors: bytebit
  * @Description:
  * @FilePath: /node-adsl/index.go
  * 引用代码请说明出处
  */
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi Node ADSL")
+}
